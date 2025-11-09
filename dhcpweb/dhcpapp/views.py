e645cb01b418adf41b84e1e0882629db6c8e5746
g333-g <stfbtf.7@gmail.com>
2025-11-09 06:16:23 +0000

    Initial commit: view.py

--- dhcpweb/dhcpapp/views.py
+++ dhcpweb/dhcpapp/views.py
@@ -1,56 +1,55 @@
 from django.shortcuts import render
 from pymongo import MongoClient
 from .forms import LeaseForm
+import time
 
 client = MongoClient("mongodb://172.31.27.209:27017/")
 db = client["dhcpdb"]
 leases = db["leases"]
 
 def mac_to_eui64(mac):
-    parts = mac.split(':')
-    eui64 = parts[:3] + ['ff', 'fe'] + parts[3:]
-    first_octet = int(eui64[0], 16) ^ 0x02
-    eui64[0] = f"{first_octet:02x}"
-    return '{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}'.format(*[int(x,16) for x in eui64])
+    p = mac.split(':')
+    e = p[:3] + ['ff','fe'] + p[3:]
+    e[0] = f"{int(e[0],16)^2:02x}"
+    return '{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}:{:02x}{:02x}'.format(*[int(x,16) for x in e])
 
-def get_next_ip(dhcp_version, mac=''):
-    if dhcp_version == "DHCPv4":
-        base = "192.168.1."
-        used = [l['assigned_ip'] for l in leases.find({"dhcp_version": "DHCPv4"})]
-        for i in range(10, 255):
-            ip = f"{base}{i}"
-            if ip not in used:
-                return ip
+def get_next_ip(v, m):
+    if v=="DHCPv4":
+        u = [l['assigned_ip'] for l in leases.find({"dhcp_version":"DHCPv4"})]
+        for i in range(10,255):
+            ip=f"192.168.1.{i}"
+            if ip not in u: return ip
     else:
-        base = "2001:db8::"
-        if mac:
-            eui = mac_to_eui64(mac)
-            ip = base + eui
-            used = [l['assigned_ip'] for l in leases.find({"dhcp_version": "DHCPv6"})]
-            if ip not in used:
-                return ip
-        return None
+        ip="2001:db8::"+mac_to_eui64(m)
+        u = [l['assigned_ip'] for l in leases.find({"dhcp_version":"DHCPv6"})]
+        if ip not in u: return ip
+    return None
 
 def index(request):
-    if request.method == "POST":
-        form = LeaseForm(request.POST)
-        if form.is_valid():
-            mac = form.cleaned_data['mac_address']
-            dhcp = form.cleaned_data['dhcp_version']
-            assigned_ip = get_next_ip(dhcp, mac)
-            lease_time = 3600
-            leases.insert_one({
-                "mac_address": mac,
-                "dhcp_version": dhcp,
-                "assigned_ip": assigned_ip,
-                "lease_time": lease_time,
+    if request.method=="POST":
+        f=LeaseForm(request.POST)
+        if f.is_valid():
+            m=f.cleaned_data['mac_address']
+            v=f.cleaned_data['dhcp_version']
+            t=3600
+            n=int(time.time())
+            e=leases.find_one({"mac_address":m,"dhcp_version":v})
+            if e and n-e.get("timestamp",0)<e.get("lease_time",t):
+                leases.update_one({"_id":e["_id"]},{"$set":{"timestamp":n}})
+                a=e["assigned_ip"]
+            else:
+                a=get_next_ip(v,m)
+                leases.insert_one({"mac_address":m,"dhcp_version":v,"assigned_ip":a,"lease_time":t,"timestamp":n})
+            return render(request,"result.html",{
+                "mac_address": m,
+                "assigned_ip": a,
+                "lease_time": f"{t} seconds"
             })
-            return render(request, "result.html", {"assigned_ip": assigned_ip})
     else:
-        form = LeaseForm()
-    return render(request, "form.html", {"form": form})
+        f=LeaseForm()
+    return render(request,"form.html",{"form":f})
 
 def leases_list(request):
-    lease_data = list(leases.find())
-    return render(request, "leases_list.html", {"lease_data": lease_data})
+    d=list(leases.find())
+    return render(request,"leases_list.html",{"lease_data":d})
 
